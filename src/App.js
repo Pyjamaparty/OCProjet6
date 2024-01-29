04950c8b1200b1fed1af6a88c756c0b25931f960
Hugo Le Port <hugo.leport@gmail.com>
2024-01-29 23:58:19 +0100

    first steps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import logo from './resources/images/LOGO.png';
 import './App.css';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+
+const Home = () => <div>Accueil</div>;
+const About = () => <div>À Propos</div>;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <img src={logo} alt="Logo" />
+      <nav>
+        <ul class="nav-list">
+          <li><Link to="/">Accueil</Link></li>
+          <li><Link to="/about">À propos</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/about" component={About} />
+        <Route path="/" component={Home} />
+      </Routes>
+    </header>
   );
 }
 
